c7c39929687d07b61f3b978a3d51425468206bbc
Yeiner P <m78tkm@gmail.com>
2025-06-24 20:10:44 -0500

    Ammo system added and working(needs a improvement in bullets counts when the player reload)

--- PreScenes/Player/player.gd
+++ PreScenes/Player/player.gd
@@ -14,9 +14,11 @@ signal ShootBullets(bullet, pos, rot, dmg)
 @onready var ShotgunSprite: Sprite2D = $BodyPart/Cont/WeaponContainer/Shotgun
 @onready var HealthBar: Control = $CanvasLayer/HealthBar
 @onready var StaminaBar: Control = $CanvasLayer/StaminaBar
+@onready var AmmoControl: Control = $CanvasLayer/AmmoControl
 
 var TimerAttackSpeed: Timer = Timer.new()
 var TimerTakeStamina: Timer = Timer.new()
+var TimerReloadWeapon: Timer = Timer.new()
 var Weapons: Array = ["fusil", "shotgun"]
 var WeaponsUsing: String = ""
 
@@ -25,11 +27,14 @@ var RunSpeed: float = 120.0
 var Health: int
 var Stamina: int
 var Acceleration: float = 50.0
-var SecForTakeStamina: int = 1
-var CostStaminaPerSSec: int = 10
+var SecForTakeStamina: float = 0.14
+var CostStaminaPerSSec: int = 1
 
 var AttackSpeedFusil: float = 0.05
 var AttackSpeedShotGun: float = 0.35
+
+var FusilReloadTime: float = 1 #Before 3.2
+var ShotgunReloadTime: float = 1 #Before 1.8
 
 var DamageFusil: float = 35
 var DamageShotGun: float = 35
@@ -47,14 +52,32 @@ var CanRun: bool = true
 var CanShoot: bool = true
 var CanShootCD: bool = true
 var CanScattering: bool = false
+var CanChangeWeapons: bool = true
+var FullReload: bool = false
+
+var FusilAmmo: Dictionary[String, int] = {
+	"MaxBullets": 35,
+	"MaxSupplier": 4,
+	"Supplier": 4,
+	"Bullets": 35
+}
+
+var ShotgunAmmo: Dictionary[String, int] = {
+	"MaxBullets": 2,
+	"MaxSupplier": 25,
+	"Supplier": 25,
+	"Bullets": 2
+}
 
 
 func _ready() -> void:
 	HealthBar.SetMaxValue(HEALTH)
 	WeaponsUsing = Weapons[0]
 	SetWeaponAtribute()
+	ShowAmmoWeapons()
 	TimerAttackSpeedSetting()
 	TimerTakeStaminaSetting()
+	TimerReloadWeaponSetting()
 
 
 func TimerAttackSpeedSetting() -> void:
@@ -73,6 +96,13 @@ func TimerTakeStaminaSetting() -> void:
 	add_child(TimerTakeStamina)
 
 
+func TimerReloadWeaponSetting() -> void:
+	TimerReloadWeapon.wait_time = FusilReloadTime
+	TimerReloadWeapon.one_shot = true
+	TimerReloadWeapon.connect("timeout", self._on_TimerReloadWeapon_timeout)
+	add_child(TimerReloadWeapon)
+
+
 func SetWeaponAtribute():
 	match WeaponsUsing:
 		"fusil":
@@ -80,15 +110,19 @@ func SetWeaponAtribute():
 			AttackSpeedGeneral = AttackSpeedFusil
 			DamageGeneral = DamageFusil
 			TimerAttackSpeed.wait_time = AttackSpeedGeneral
+			TimerReloadWeapon.wait_time = FusilReloadTime
 			CanScattering = false
 		"shotgun":
 			WeaponsUsing = Weapons[1]
 			AttackSpeedGeneral = AttackSpeedShotGun
 			DamageGeneral = DamageShotGun
 			TimerAttackSpeed.wait_time = AttackSpeedGeneral
+			TimerReloadWeapon.wait_time = ShotgunReloadTime
 			CanScattering = true
+	
+	ShowAmmoWeapons()
 
-
+@warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	if !IsDeath:
 		AimWeapons()
@@ -150,24 +184,39 @@ func CheckIsMoving(val: Vector2) -> void:
 
 
 func ChangeWeapon():
-	if Input.is_action_just_pressed("1"):
-		WeaponsUsing = Weapons[0]
-		FusilSprite.visible = true
-		ShotgunSprite.visible = false
-		SetWeaponAtribute()
-	elif Input.is_action_just_pressed("2"):
-		WeaponsUsing = Weapons[1]
-		FusilSprite.visible = false
-		ShotgunSprite.visible = true
-		SetWeaponAtribute()
+	if CanChangeWeapons:
+		if Input.is_action_just_pressed("1"):
+			WeaponsUsing = Weapons[0]
+			FusilSprite.visible = true
+			ShotgunSprite.visible = false
+			AmmoControl.get_node("Weapon").get_node("Fusil").visible = true
+			AmmoControl.get_node("Weapon").get_node("Shotgun").visible = false
+			TimerAttackSpeed.stop()
+			FullReload = false
+			SetWeaponAtribute()
+		elif Input.is_action_just_pressed("2"):
+			WeaponsUsing = Weapons[1]
+			FusilSprite.visible = false
+			ShotgunSprite.visible = true
+			AmmoControl.get_node("Weapon").get_node("Fusil").visible = false
+			AmmoControl.get_node("Weapon").get_node("Shotgun").visible = true
+			TimerAttackSpeed.stop()
+			FullReload = false
+			SetWeaponAtribute()
+	
+	if Input.is_action_just_pressed("r"):
+		TimerReloadWeapon.start()
+		FullReload = true
 
 
 func ShootWeapons():
 	match WeaponsUsing:
 		"fusil":
-			if Input.is_action_pressed("left_click") and CanShoot and CanShootCD:
+			if Input.is_action_pressed("left_click") and CanShoot and CanShootCD and GetBulletsCont(FusilAmmo) > 0:
 				CanShootCD = false
 				TimerAttackSpeed.start()
+				SpendBullet(FusilAmmo)
+				ShowAmmoWeapons()
 				
 				if !CanScattering:
 					emit_signal("ShootBullets", bullet, BodyPart.get_node("Marker2D").global_position, BodyPart.rotation_degrees + 90, DamageGeneral)
@@ -184,9 +233,11 @@ func ShootWeapons():
 						var current_angle = start_angle + (angle_step * i)
 						emit_signal("ShootBullets", bullet, BodyPart.get_node("Marker2D").global_position, current_angle, DamageGeneral)
 		"shotgun":
-			if Input.is_action_just_pressed("left_click") and CanShoot and CanShootCD:
+			if Input.is_action_just_pressed("left_click") and CanShoot and CanShootCD and GetBulletsCont(ShotgunAmmo) > 0:
 				CanShootCD = false
 				TimerAttackSpeed.start()
+				SpendBullet(ShotgunAmmo)
+				ShowAmmoWeapons()
 				
 				if !CanScattering:
 					emit_signal("ShootBullets", bullet, BodyPart.get_node("Marker2D").global_position, BodyPart.rotation_degrees + 90, DamageGeneral)
@@ -241,3 +292,82 @@ func CheckStaminaPerSecond() -> void:
 
 func _on_health_bar_empty_bar() -> void:
 	IsDeath = true
+
+
+func ShowAmmoWeapons() -> void:
+	AmmoControl.get_node("AmmoCont").modulate = Color(1, 1 ,1)
+	match WeaponsUsing:
+		"fusil":
+			AmmoControl.get_node("AmmoCont").get_node("LabelSupplier").text = str(FusilAmmo.Supplier)
+			AmmoControl.get_node("AmmoCont").get_node("LabelBullets").text = str(FusilAmmo.Bullets)
+			
+			if FusilAmmo.Supplier <= 0:
+				AmmoControl.get_node("AmmoCont").get_node("LabelSupplier").modulate = Color(1, 0 ,0)
+			else:
+				AmmoControl.get_node("AmmoCont").get_node("LabelSupplier").modulate = Color(1, 1 ,1)
+			
+			if FusilAmmo.Bullets <= 0:
+				AmmoControl.get_node("AmmoCont").get_node("LabelBullets").modulate = Color(1, 0 ,0)
+			else:
+				AmmoControl.get_node("AmmoCont").get_node("LabelBullets").modulate = Color(1, 1 ,1)
+			
+			if FusilAmmo.Bullets <= 0 and FusilAmmo.Supplier <= 0:
+				AmmoControl.get_node("AmmoCont").modulate = Color(1, 0 ,0)
+			
+		"shotgun":
+			AmmoControl.get_node("AmmoCont").get_node("LabelSupplier").text = str(ShotgunAmmo.Supplier)
+			AmmoControl.get_node("AmmoCont").get_node("LabelBullets").text = str(ShotgunAmmo.Bullets)
+			
+			if ShotgunAmmo.Supplier <= 0:
+				AmmoControl.get_node("AmmoCont").get_node("LabelSupplier").modulate = Color(1, 0 ,0)
+			else:
+				AmmoControl.get_node("AmmoCont").get_node("LabelSupplier").modulate = Color(1, 1 ,1)
+			
+			if ShotgunAmmo.Bullets <= 0:
+				AmmoControl.get_node("AmmoCont").get_node("LabelBullets").modulate = Color(1, 0 ,0)
+			else:
+				AmmoControl.get_node("AmmoCont").get_node("LabelBullets").modulate = Color(1, 1 ,1)
+			
+			if ShotgunAmmo.Bullets <= 0 and ShotgunAmmo.Supplier <= 0:
+				AmmoControl.get_node("AmmoCont").modulate = Color(1, 0 ,0)
+
+
+func SpendBullet(dic: Dictionary) -> void:
+	dic.Bullets -= 1
+
+
+func SpendSupplier(dic: Dictionary) -> void:
+	dic.Supplier -= 1
+
+
+func GetBulletsCont(dic: Dictionary) -> int:
+	return dic.Bullets
+
+
+func GetSupplierCont(dic: Dictionary) -> int:
+	return dic.Supplier
+
+
+func AddSupplier(dic: Dictionary, amount: int) -> void:
+	if dic.Supplier < dic.MaxSupplier:
+		dic.Supplier += amount
+	if dic.Supplier > dic.MaxSupplier:
+		dic.Supplier = dic.MaxSupplier
+
+
+func ReloadWeapon(dic: Dictionary) -> void:
+	if dic.Supplier > 0:
+		dic.Supplier -= 1
+		dic.Bullets = dic.MaxBullets
+
+
+func _on_TimerReloadWeapon_timeout() -> void:
+	if FullReload:
+		FullReload = false
+		match WeaponsUsing:
+			"fusil":
+				ReloadWeapon(FusilAmmo)
+			"shotgun":
+				ReloadWeapon(ShotgunAmmo)
+	
+	ShowAmmoWeapons()
